2575b407b7a9dbe386d6e2b1162ed30fe9edf026
guzus <50664161+guzus@users.noreply.github.com>
2021-10-22 19:03:15 +0900

    simple structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # beauty-frog
 NPM module that supports beautiful console-logging experience for Node JS
+
+# installation
+`npm i beauty-frog --save`
---
+++ examples/index.js
@@ -0,0 +1,9 @@
+const { horizontalView } = require("beauty-frog");
+
+horizontalView({
+  elements: [
+    "hi this is beauty-frog.\nHow do you like this package?",
+    "this is mind-blowing.\nI gotta tell my mom about this.\nI'm loving it.",
+  ],
+  blankCount: 5,
+});
